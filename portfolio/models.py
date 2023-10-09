44096ac9c8d1dc05f1835146baa31571a4b3ccdc
AmayaXitado <amaya78951@gmail.com>
2023-10-09 09:58:21 -0500

    Update Imagen

--- portfolio/models.py
+++ portfolio/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Project(models.Model):
     title = models.CharField(max_length=200, verbose_name="Título")
     description = models.TextField(verbose_name="Descripción")
-    image = models.ImageField(upload_to='project_images/')
+    image = models.ImageField(verbose_name="Imagen",upload_to='project_images/')
     created = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de creación")
     update = models.DateTimeField(auto_now=True, verbose_name="Fecha de edición")
 
